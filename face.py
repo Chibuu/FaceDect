321a647be4dce953f4bd616ce215d15870635859
Chibu <chibueze.ukachi@epfl.ch>
2016-12-22 21:43:21 +0100

    Working implementation of face detection

--- face.py
+++ face.py
@@ -1,14 +1,22 @@
-''' Rasperry pi implementation of the paper:
+''' Rasperry pi implementation of face detection via skin segmentation of the pper
 "A simple and accurate face detection algorithm in complex background"
-by Yu-Tang Pai, Shanq-Jang Ruan, Mon-Chau Shie, Yi-Chi Liu ''' 
+by Yu-Tang Pai, Shanq-Jang Ruan, Mon-Chau Shie, Yi-Chi Liu 
+
+Author: Chibueze Ukachi''' 
+
+
 #numpy gives matlab matrix functionality
 import numpy as np
-#PILLOW allows us to open and show the images
+#PILLOW allows us to open and show the images, aswell as draw rectangles
 from PIL import Image
+from PIL import ImageDraw
 
+#open image file
 #img = Image.open('ron.jpg')
 img = Image.open('nadal.jpg')
+#convert to numpy array
 img = np.asarray(img)
+original_image = img
 img.astype(dtype = 'double')
 
 #retrieving RGB components
@@ -83,6 +91,7 @@ for i in range(1,height+1 - filter_size):
         skin_wnr[i:i+filter_size, j:j+filter_size]=(temp_sum>=13);
 
 
+
 img = Image.fromarray(np.uint8(img))
 
 img.show(title= 'Original Image')
@@ -99,3 +108,133 @@ skin.show(title = 'Skin detection complete')
 skin_with_noise_removal = Image.fromarray(np.uint8(255*skin_wnr))
 
 skin_with_noise_removal.show('After low pass filter')
+
+bw = np.uint8(skin_wnr)
+
+#downsampling is needed as the connected component labelling is recursive 
+#python crashes in matrix is too large
+down_sample = 5
+bw = bw[::down_sample,::down_sample]
+
+#collect image size after downsampling
+bw_row,bw_col = bw.shape
+
+'''I manually ported the connected component labelling from Matlab to python with slight modifications
+Credit to :michael scheinfeild
+https://ch.mathworks.com/matlabcentral/fileexchange/38010-connected-component-labeling-like-bwlabel
+'''
+def findConnectedLabels(L,startLabel,bwcur,ir,ic,m,n):
+
+    a = bwcur[ir+1, ic]
+    b = bwcur[ir-1, ic]   
+    c = bwcur[ir, ic+1] 
+    d = bwcur[ir, ic-1]
+
+    aa = L[ir+1, ic]
+    bb = L[ir-1, ic]
+    cc = L[ir, ic+1] 
+    dd = L[ir, ic-1]
+
+    if((a==1) and (aa==0)):
+        L[ir+1, ic]=startLabel
+        L  = findConnectedLabels(L,startLabel,bwcur,ir+1,ic,m,n)
+
+    if((b==1) and (bb==0)):
+        L[ir-1, ic]=startLabel
+        L  = findConnectedLabels(L,startLabel,bwcur,ir-1,ic,m,n)
+
+    if((c==1)and(cc==0)):
+        L[ir, ic+1]=startLabel
+        L  = findConnectedLabels(L,startLabel,bwcur,ir,ic+1,m,n)
+
+    if((d==1)and (dd==0)):
+        L[ir, ic-1]=startLabel
+        L  = findConnectedLabels(L,startLabel,bwcur,ir,ic-1,m,n)
+
+
+    return L
+
+def detect(bw):
+    
+    m,n = bw.shape
+    emp1= np.zeros((1,n),dtype = int)
+    t1 = np.append(emp1,bw,axis=0)
+    bwnnew = np.append(t1,emp1,axis=0)
+
+    emp2 = np.zeros((m+2,1),dtype=int)
+    t2 = np.append(emp2,bwnnew,axis=1)
+    bwnnew = np.append(t2,emp2,axis=1)
+
+
+    L = np.zeros(bwnnew.shape,dtype = int)
+
+    startLabel = 1
+
+    for ir in range(2,m+2):
+        for ic in range(2,n+2):
+            curdata = bwnnew[ir,ic]
+            #print(curdata)
+            lc = L[ir,ic]
+            if (curdata ==1) and(lc ==0):
+                L[ir,ic]= startLabel
+                L = findConnectedLabels(L,startLabel,bwnnew,ir,ic,m,n);
+                startLabel = startLabel  + 1
+                
+    
+    return L[1:m+1,1:n+1]
+    
+#I added face prop as really small groups of peoples that are connected are unlikely to be a face
+face_prop = 0.002 *bw_row*bw_col
+
+#collecting the labels
+bw_label = detect(bw)
+
+
+#note that after the connected component labelling there can be a huge number of labels 
+#as isolated pixels are given labels 
+final_label = []
+start_pos_x = []
+start_pos_y = []
+height_list = []
+width_list = []
+
+#these are from tests that I performed. 
+low_threshold = 1.0
+high_threshold = 1.5
+
+#for every label
+for label in range(1,np.amax(bw_label)):
+    #ignore it if it could be a smudge
+    if len(bw_label[bw_label == label]) > face_prop:
+        temp_bw_label = np.array(bw_label, copy=True)
+        temp_bw_label[temp_bw_label!=label] = 0
+        row, col = np.where(temp_bw_label==label)
+        #these are the positions required for the bounding box
+        start_x = np.amin(col)
+        start_y = np.amin(row)
+        width = np.amax(col) -  start_x + 1
+        height = np.amax(row) - start_y + 1
+        
+        #take lev
+        if ((height/width > low_threshold) and (height/width <high_threshold)):
+            #tcurrent label number
+            final_label.append(label)
+            #these are the positions required for the bounding box for each iteration
+            start_pos_x.append(start_x)
+            start_pos_y.append(start_y)
+            height_list.append(height)
+            width_list.append(width)
+
+for i in range(len(final_label)):
+    label_num = final_label[i]
+    start_x = start_pos_x[i]
+    start_y = start_pos_y[i]
+    height = height_list[i]
+    width = width_list[i]
+    img1 = Image.fromarray(original_image)
+    draw = ImageDraw.Draw(img1)
+    
+    draw.rectangle([down_sample*start_x,down_sample*start_y,down_sample*(start_x+width),
+                    down_sample*(start_y+height)], fill=None, outline="red")
+    img1.show()
+    
